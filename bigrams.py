d573a6608b94cbb35280c2da37aef6253badd566
Toby Donaldson <tjdonaldson@gmail.com>
2019-02-16 11:14:05 -0800

    cleaned up bigram.py

--- bigrams.py
+++ bigrams.py
@@ -1,33 +1,39 @@
 # bigrams.py
 
 #
-# This code demonstrates how bigrams, pairs of consecutive words, can be used
+# This code demonstrates how bigrams (pairs of consecutive words) can be used
 # to create auto-complete suggestions for the next word to type.
 #
-# The idea is to take some text (lots and lots of it!) and count how many
-# times each bigram occurs. Then, for instance, if someone types the word 'I',
-# we can suggest as the next word the second word in the most frequent bigram
-# starting with 'I'.
+# The idea is to take some text (usually lots and lots of it!) and count how
+# many times each bigram occurs. Then, for instance, if someone types the word
+# 'I', we can suggest as the next word the second word in the most frequent
+# bigram starting with 'I'.
 #
 # This program was written to be as easy to understand as possible. It's *not*
 # particularly efficient, and could be sped up and improved in many ways.
 #
 
+#
 # Returns a list of all the words in the given file.
+#
 def get_words(fname):
 	words = open(fname, 'r').read().lower().split()
 	return words
 
+#
 # Converts a list of words into a list of bigrams
 # e.g. ['once', 'upon', 'a', 'time'] gives the bigrams
 # [('once', 'upon'), ('upon', 'a'), ('a', 'time')]
+#
 def make_bigrams(words):
 	result = []
 	for i in range(1, len(words)):
 		result.append((words[i-1], words[i]))
 	return result
 
+#
 # Given a list of bigrams, returns a dictionary of bigrams and their counts.
+#
 def count_bigrams(bigrams):
 	result = {}
 	for bg in bigrams:
@@ -37,21 +43,27 @@ def count_bigrams(bigrams):
 			result[bg] = 1
 	return result
 
+#
 # Given a dictionary of bigram counts, returns a list of (count, bigram) pairs
 # in order from highest count to lowest count.
+#
 def sort_bigram_counts(bg_count):
 	result = [(bg_count[w], w) for w in bg_count]
 	result.sort()
 	result.reverse()
 	return result
 
+#
 # Returns the total number of bigrams in the given sorted list of (count,
 # bigram) pairs where w is the first word of the bigram.
+#
 def num_start_with(w, counts):
 	return sum(cp[0] for cp in counts if cp[1][0] == w)
 
+#
 # Given a words and a sorted list of (count, bigram) pairs, returns a list of
 # the words that most frequently follow word.
+#
 def suggest_next(word, counts, num_suggestions=5):
 	# get first num_suggestions (count, bigram) pairs that start with word
 	result = [(c, bg) for (c, bg) in counts if bg[0] == word]
@@ -71,10 +83,11 @@ def suggest_next(word, counts, num_suggestions=5):
 # 				break
 # 	return result
 
-# Shows most popular suggestions (and their counts) for word w.
-# counts is a sorted list of (count, bigram) pairs
+#
+# Shows most popular suggestions (and their counts) for word w. counts is a
+# sorted list of (count, bigram) pairs
+#
 def report_info(w, counts):
-	# get all the bigrams that start with w
 	w_total = num_start_with(w, counts)
 	w_pairs = suggest_next(w, counts)
 	print(f'{w_total} bigrams start with "{w}"')
@@ -83,17 +96,20 @@ def report_info(w, counts):
 	for (c, bg) in w_pairs:
 		print(f'{bg[0]} {bg[1]} ({100*c/w_total:.1f}%, {c})')
 
-# Returns the word that appears most frequently after w.
-# counts is a sorted list of (count, bigram) pairs
+#
+# Returns the word that appears most frequently after w. counts is a sorted
+# list of (count, bigram) pairs
+#
 def get_most_freq_after(w, counts):
 	for (c, bg) in counts:
 		if bg[0] == w:
 			return bg[1]
 	return ''
 
-# Returns sequence of most likely words beginning with start_word.
-# Can result in some amusing sentences.
-# counts is a sorted list of (count, bigram) pairs
+#
+# Returns sequence of most likely words beginning with start_word. Can result
+# in some amusing sentences. counts is a sorted list of (count, bigram) pairs
+#
 def word_sequence(start_word, counts, length=5):
 	result = [start_word]
 	w = start_word
@@ -103,24 +119,26 @@ def word_sequence(start_word, counts, length=5):
 		w = next_word
 	return result
 
+#
 # Provides a simple interactive way to get information about bigrams.
-# 
-# e.g. 
-# --> info tooth
-# 13 bigrams start with "tooth"
-# tooth on (15.4%, 2)
-# tooth of (15.4%, 2)
-# tooth is (15.4%, 2)
-# tooth in (15.4%, 2)
-# tooth will (7.7%, 1)
-# --> seq tooth
-# tooth on the king henry. i
-# --> seq crown 
-# crown of the king henry. i
-# --> seq swim
-# swim like a man of the
+# Here's a sample run:
+#  
+#    --> info tooth
+#    13 bigrams start with "tooth"
+#    tooth on (15.4%, 2)
+#    tooth of (15.4%, 2)
+#    tooth is (15.4%, 2)
+#    tooth in (15.4%, 2)
+#    tooth will (7.7%, 1)
+#    --> seq tooth
+#    tooth on the king henry. i
+#    --> seq crown 
+#    crown of the king henry. i
+#    --> seq swim
+#    swim like a man of the
 #
 # counts is a sorted list of (count, bigram) pairs
+#
 def interact(counts):
 	print()
 	print('Commands:')
@@ -162,9 +180,3 @@ if __name__ == '__main__':
 	print('... done sorting')
 
 	interact(sorted_counts)
-
-	# # # look at suggestions for a few words
-	# test_words = 'i why who good happy love hate sad bad evil king queen god man woman romeo juliet'.split()
-	# for w in test_words:
-	# 	print(word_sequence(w, sorted_counts))
-	# 	# report_info(w, sorted_counts)
